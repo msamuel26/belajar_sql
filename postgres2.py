82220de26ef907ad5b8aca15f4b3e6a2fe015f82
Moses Samuel <msamuelmulia@gmail.com>
2022-12-21 10:51:12 +0700

    Mempelajari add banyak data ke tabel dalam suatu database dengan 1 line code (dengan penggunaan metode yang lebih efisien), yaitu menggunakan fungsi bulk_save_objects()

--- postgres2.py
+++ postgres2.py
@@ -41,22 +41,16 @@ new_customer2 = Customer(first_name='David', last_name='Robinson', age=22, count
 new_customer3 = Customer(first_name='John', last_name='Reinhardt', age=25, country='UK')
 new_customer4 = Customer(first_name='Betty', last_name='Doe', age=28, country='UAE')
 
+print("masukkan banyak data ke dalam tabel database customer dalam 1 line code")
 session.bulk_save_objects([new_customer, new_customer1, new_customer2,
              new_customer3, new_customer4], return_defaults=True)
 session.commit()
 
-# 2022-12-20 12:01:09,573 INFO sqlalchemy.engine.Engine BEGIN (implicit)
-# 2022-12-20 12:01:09,573 INFO sqlalchemy.engine.Engine INSERT INTO customer (first_name, last_name, age, country) VALUES (?, ?, ?, ?)
-# 2022-12-20 12:01:09,573 INFO sqlalchemy.engine.Engine [generated in 0.00127s] ('John', 'Doe', 31, 'USA')
-# 2022-12-20 12:01:09,581 INFO sqlalchemy.engine.Engine INSERT INTO customer (first_name, last_name, age, country) VALUES (?, ?, ?, ?)
-# 2022-12-20 12:01:09,581 INFO sqlalchemy.engine.Engine [cached since 0.009067s ago] ('Robert', 'Luna', 22, 'USA')
-# 2022-12-20 12:01:09,581 INFO sqlalchemy.engine.Engine INSERT INTO customer (first_name, last_name, age, country) VALUES (?, ?, ?, ?)
-# 2022-12-20 12:01:09,581 INFO sqlalchemy.engine.Engine [cached since 0.01108s ago] ('David', 'Robinson', 22, 'UK')
-# 2022-12-20 12:01:09,581 INFO sqlalchemy.engine.Engine INSERT INTO customer (first_name, last_name, age, country) VALUES (?, ?, ?, ?)
-# 2022-12-20 12:01:09,581 INFO sqlalchemy.engine.Engine [cached since 0.01237s ago] ('John', 'Reinhardt', 25, 'UK')
-# 2022-12-20 12:01:09,581 INFO sqlalchemy.engine.Engine INSERT INTO customer (first_name, last_name, age, country) VALUES (?, ?, ?, ?)
-# 2022-12-20 12:01:09,581 INFO sqlalchemy.engine.Engine [cached since 0.01376s ago] ('Betty', 'Doe', 28, 'UAE')
-# 2022-12-20 12:01:09,592 INFO sqlalchemy.engine.Engine COMMIT
+# 2022-12-21 10:40:49,962 INFO sqlalchemy.engine.Engine BEGIN (implicit)
+# 2022-12-21 10:40:49,962 INFO sqlalchemy.engine.Engine INSERT INTO customer (id, first_name, last_name, age, country) VALUES (nextval('customer_id_seq'), %(first_name)s, %(last_name)s, %(age)s, %(country)s) RETURNING customer.id
+# 2022-12-21 10:40:49,962 INFO sqlalchemy.engine.Engine [generated in 0.00087s] ({'first_name': 'John', 'last_name': 'Doe', 'age': 31, 'country': 'USA'}, {'first_name': 'Robert', 'last_name': 
+# 'Luna', 'age': 22, 'country': 'USA'}, {'first_name': 'David', 'last_name': 'Robinson', 'age': 22, 'country': 'UK'}, {'first_name': 'John', 'last_name': 'Reinhardt', 'age': 25, 'country': 'UK'}, {'first_name': 'Betty', 'last_name': 'Doe', 'age': 28, 'country': 'UAE'})
+# 2022-12-21 10:40:49,969 INFO sqlalchemy.engine.Engine COMMIT
 
 print("insert multiple data ke dalam order")
 new_order1 = Order(item='Keyboard', amount=400, customer_id=new_customer3.id)
@@ -65,44 +59,22 @@ new_order3= Order(item='Monitor', amount=12000, customer_id=new_customer2.id)
 new_order4 = Order(item='Keyboard', amount=400, customer_id=new_customer.id)
 new_order5 = Order(item='Mousepad', amount=250, customer_id=new_customer1.id)
 
+print("masukkan banyak data ke dalam tabel database order dalam 1 line code")
 session.bulk_save_objects([new_order1, new_order2, new_order3, new_order4])
 session.commit()
 
-# 2022-12-20 12:01:09,602 INFO sqlalchemy.engine.Engine BEGIN (implicit)
-# 2022-12-20 12:01:09,612 INFO sqlalchemy.engine.Engine SELECT customer.id AS customer_id, customer.first_name AS customer_first_name, customer.last_name AS customer_last_name, customer.age AS customer_age, customer.country AS customer_country
-# FROM customer
-# WHERE customer.id = ?
-# 2022-12-20 12:01:09,612 INFO sqlalchemy.engine.Engine [generated in 0.00124s] (9,)
-# 2022-12-20 12:01:09,612 INFO sqlalchemy.engine.Engine SELECT customer.id AS customer_id, customer.first_name AS customer_first_name, customer.last_name AS customer_last_name, customer.age AS customer_age, customer.country AS customer_country
-# FROM customer
-# WHERE customer.id = ?
-# 2022-12-20 12:01:09,612 INFO sqlalchemy.engine.Engine [cached since 0.004633s ago] (8,)        
-# 2022-12-20 12:01:09,612 INFO sqlalchemy.engine.Engine SELECT customer.id AS customer_id, customer.first_name AS customer_first_name, customer.last_name AS customer_last_name, customer.age AS customer_age, customer.country AS customer_country
-# FROM customer
-# WHERE customer.id = ?
-# 2022-12-20 12:01:09,622 INFO sqlalchemy.engine.Engine [cached since 0.008788s ago] (6,)
-# 2022-12-20 12:01:09,622 INFO sqlalchemy.engine.Engine SELECT customer.id AS customer_id, customer.first_name AS customer_first_name, customer.last_name AS customer_last_name, customer.age AS customer_age, customer.country AS customer_country
-# FROM customer
-# WHERE customer.id = ?
-# 2022-12-20 12:01:09,622 INFO sqlalchemy.engine.Engine [cached since 0.0113s ago] (7,)
-# 2022-12-20 12:01:09,622 INFO sqlalchemy.engine.Engine INSERT INTO "order" (item, amount, customer_id) VALUES (?, ?, ?)
-# 2022-12-20 12:01:09,622 INFO sqlalchemy.engine.Engine [generated in 0.00112s] ('Keyboard', 400, 9)
-# 2022-12-20 12:01:09,632 INFO sqlalchemy.engine.Engine INSERT INTO "order" (item, amount, customer_id) VALUES (?, ?, ?)
-# 2022-12-20 12:01:09,632 INFO sqlalchemy.engine.Engine [cached since 0.00519s ago] ('Mouse', 300, 9)
-# 2022-12-20 12:01:09,632 INFO sqlalchemy.engine.Engine INSERT INTO "order" (item, amount, customer_id) VALUES (?, ?, ?)
-# 2022-12-20 12:01:09,632 INFO sqlalchemy.engine.Engine [cached since 0.006669s ago] ('Monitor', 
-# 12000, 8)
-# 2022-12-20 12:01:09,632 INFO sqlalchemy.engine.Engine INSERT INTO "order" (item, amount, customer_id) VALUES (?, ?, ?)
-# 2022-12-20 12:01:09,632 INFO sqlalchemy.engine.Engine [cached since 0.008336s ago] ('Keyboard', 400, 6)
-# 2022-12-20 12:01:09,632 INFO sqlalchemy.engine.Engine INSERT INTO "order" (item, amount, customer_id) VALUES (?, ?, ?)
-# 2022-12-20 12:01:09,632 INFO sqlalchemy.engine.Engine [cached since 0.01024s ago] ('Mousepad', 
-# 250, 7)
-# 2022-12-20 12:01:09,632 INFO sqlalchemy.engine.Engine COMMIT
+# 2022-12-21 10:45:33,308 INFO sqlalchemy.engine.Engine BEGIN (implicit)
+# 2022-12-21 10:45:33,308 INFO sqlalchemy.engine.Engine INSERT INTO "order" (id, item, amount, customer_id) VALUES (nextval('order_id_seq'), %(item)s, %(amount)s, %(customer_id)s)
+# 2022-12-21 10:45:33,308 INFO sqlalchemy.engine.Engine [generated in 0.00072s] ({'item': 'Keyboard', 'amount': 400, 'customer_id': 59}, {'item': 'Mouse', 'amount': 300, 'customer_id': 59}, {'item': 'Monitor', 'amount': 12000, 'customer_id': 58}, {'item': 'Keyboard', 'amount': 400, 'customer_id': 56})
+# 2022-12-21 10:45:33,316 INFO sqlalchemy.engine.Engine COMMIT
 
-
+print("engine execute")
 engine.execute("insert into customer (id, first_name, last_name, age, country) values (6, 'Setip', 'Cengiri', 7, 'IDN')")
+
+# 2022-12-21 10:45:33,316 INFO sqlalchemy.engine.Engine insert into customer (id, first_name, last_name, age, country) values (6, 'Setip', 'Cengiri', 7, 'IDN')
+# 2022-12-21 10:45:33,316 INFO sqlalchemy.engine.Engine [raw sql] {}
+# 2022-12-21 10:45:33,316 INFO sqlalchemy.engine.Engine COMMIT
 
 print("menutup sesi transaction/koneksi ke database")
 # bertujuan untuk menutup sesi transaction/koneksi ke database
 session.close()
-
