1402573e643962322e37304b66b4507d81a3e022
Moses Samuel <msamuelmulia@gmail.com>
2023-02-01 05:34:13 +0700

    Menambahkan tabel Shippings via SQLAlchemy dan Pydantic

--- api2.py
+++ api2.py
@@ -25,6 +25,11 @@ class OrderCreate(BaseModel):
     amount: int = Field(..., example=300)
     customer_id: int = Field(..., example=1)
 
+# Create a Pydantic model for the Shipping
+class ShippingCreate(BaseModel):
+    status: str = Field(..., example='Pending')
+    customer: int = Field(..., example=3)
+
 # Create a SQLAlchemy model for the Customer
 class Customer(Base):
     __tablename__ = "customers"
@@ -40,6 +45,13 @@ class Order(Base):
     id = Column(Integer, primary_key=True, index=True)
     item = Column(String)
     amount = Column(Integer)
+    customer_id = Column(Integer)
+
+# Create a SQLAlchemy model for the Order
+class Shipping(Base):
+    __tablename__ = 'shippings'
+    id = Column(Integer, primary_key=True, index=True)
+    status = Column(String)
     customer_id = Column(Integer)
 
 # Create the "users" table in the database
@@ -68,6 +80,16 @@ async def create_order(order: OrderCreate):
     session.close()
     return {"message": "Order created successfully"}
 
+# Define a route to create a new shipping in the database
+@app.post("/shippings/") 
+async def create_shipping(shipping: ShippingCreate):
+    session = SessionLocal()
+    new_shipping = Shipping(**shipping.dict())
+    session.add(new_shipping)
+    session.commit()
+    session.close()
+    return {"message": "Shipping created successfully"}
+
 # Define a route to list all customers from the database
 @app.get("/customers/", response_class=JSONResponse)
 async def list_customers():
@@ -83,6 +105,14 @@ async def list_orders():
     orders = session.query(Order).all()
     session.close()
     return JSONResponse(content= [{"id": order.id, "item": order.item, "amount": order.amount, "customer_id": order.customer_id} for order in orders])
+
+# Define a route to list all shippings from the database
+@app.get("/shippings/", response_class=JSONResponse)
+async def list_shippings():
+    session = SessionLocal()
+    shippings = session.query(Order).all()
+    session.close()
+    return JSONResponse(content= [{"id": shipping.id, "status": shipping.status, "customer_id": shipping.customer_id} for shipping in shippings])
 
 @app.get("/")
 async def root():
